75dfbe285471d26da4a45622ee6a14a48b70fade
Lewis <lewis@spellcraft.org>
2024-04-15 04:55:25 -0700

    chore: concurrency 10, max 100 (GCP rate limit incr)

--- src/backoff.mjs
+++ src/backoff.mjs
@@ -7,12 +7,11 @@ export async function backoff(action, maxRetries = 6, initialDelay = 15) {
       const results = await action();
       return results;
     } catch (e) {
-      retries++;
-
       if (retries === maxRetries) {
         throw new Error('Max retries exceeded.');
       }
 
+      retries++;
       console.error(e.message);
       console.error(`Error. Retrying in ${delay}s. [Attempt ${retries}/${maxRetries}]`);
 
