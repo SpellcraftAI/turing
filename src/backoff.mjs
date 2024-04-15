65b47e82dcc2e1a16b18782920ee50e348cd13ae
Lewis <lewis@spellcraft.org>
2024-04-15 06:22:53 -0700

    chore: reduce to 7 concurrent

--- src/backoff.mjs
+++ src/backoff.mjs
@@ -2,21 +2,21 @@ export async function backoff(action, maxRetries = 6, initialDelay = 15) {
   let retries = 0;
   let delay = initialDelay;
 
-  while (retries < maxRetries) {
+  while (true) {
     try {
       const results = await action();
       return results;
     } catch (e) {
-      if (retries === maxRetries) {
-        throw new Error('Max retries exceeded.');
-      }
-
       retries++;
       console.error(e.message);
-      console.error(`Error. Retrying in ${delay}s. [Attempt ${retries}/${maxRetries}]`);
 
-      await new Promise(resolve => setTimeout(resolve, 1000 * delay));
-      delay *= 2;
+      if (retries <= maxRetries) {
+        console.error(`Error. Retrying in ${delay}s. [Attempt ${retries}/${maxRetries}]`);
+        await new Promise(resolve => setTimeout(resolve, 1000 * delay));
+        delay *= 2;
+      } else {
+        throw new Error('Max retries exceeded.');
+      }
     }
   }
 }
