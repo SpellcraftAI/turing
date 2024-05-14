f35668ab2e4170c1d33ca7e1339d17c8f1a3d718
C. T. J. Lewis <lewis@spellcraft.org>
2024-05-13 20:55:47 -0700

    chore: tape that does not use binary cannot handle 5 * 5 digit

--- programs/arithmetic/eval.ts
+++ programs/arithmetic/eval.ts
@@ -19,8 +19,8 @@ export default function multiply(a: number | string, b: number | string): string
   const output: number[] = new Array(digitsA.length + digitsB.length).fill(0)
 
   logs.push("START")
-  logs.push(`A ${toPositional(A)} ${A}`)
-  logs.push(`B ${toPositional(B)} ${B}`)
+  logs.push(`${toPositional(A, "A")} ${A}`)
+  logs.push(`${toPositional(B, "B")} ${B}`)
   logs.push(`${A} * ${B}`)
   digitsA.reverse()
   digitsB.reverse()
@@ -41,46 +41,52 @@ export default function multiply(a: number | string, b: number | string): string
 
   // Perform the multiplication
   for (let i = 0; i < digitsB.length; i++) {
-    logs.push(`OUTPUT ${toPositional(output.join(""), "O")}`)
+    logs.push(`LOOP B ${i}/${digitsB.length - 1}`)
+    const digitB = digitsB[i]
+
+    logs.push(`${toPositional(output.join(""), "O")}`)
+    logs.push(`${toPositional(digitsA.join(""), "A")}`)
+    logs.push(`${toPositional(digitsB.join(""), "B")}`)
+
     for (let j = 0; j < digitsA.length; j++) {
+      logs.push(`LOOP A ${j}/${digitsA.length - 1} B ${i}/${digitsB.length - 1}`)
+    
       const digitA = digitsA[j]
-      const digitB = digitsB[i]
       const product = digitA * digitB
       const position = i + j
 
-      logs.push(`B:${i}/${digitsB.length-1} A:${j}/${digitsA.length-1}`)
-      logs.push(`${i} + ${j} = ${position}`)
-      logs.push(`${i}:${digitB} * ${j}:${digitA} = ${product}`)
+      // logs.push(`B:${i}/${digitsB.length - 1} A:${j}/${digitsA.length - 1}`)
+      logs.push(`${i} ${j} ${position} ${position + 1}`)
+      logs.push(`B${i}:${digitB} * A${j}:${digitA} = ${product}`)
 
       const current = output[position]
       const next = output[position] + product
-      logs.push(`${position}:${current} + ${product} = O${position}:${next}`)
-      
+      logs.push(`O${position}:${current} + ${product} = ${next}`)
+      // logs.push(`O${position}:${current} + ${product} = O${position}${next}`)
+
       output[position] = next
 
       // Handle carry over
       let k = position
       if (output[k] >= 10) {
         logs.push("CARRY")
-      
         while (output[k] >= 10) {
           const head = output[k]
           const next = output[k + 1]
-  
-          logs.push(`O${k}:${head} O${k + 1}:${next}`)
           const tens = Math.floor(output[k] / 10)
           const remainder = output[k] % 10
+
+          logs.push(`O${k}:${head} O${k + 1}:${next}`)
           logs.push(`O${k}:${head} ${tens} ${remainder}`)
-          logs.push(`O${k + 1}:${next} + ${tens} = O${next + tens}`)
-  
+          logs.push(`O${k + 1}:${next} + ${tens} = O${k + 1}:${next + tens}`)
+
           output[k + 1] += tens
           output[k] = remainder
-          logs.push(`SET O${k}:${remainder}`)
-          logs.push(`O${k}:${output[k]} O${k + 1}:${output[k + 1]}`)
+          logs.push(`SET O${k}:${output[k]} O${k + 1}:${output[k + 1]}`)
           k++
         }
-
-        logs.push("DONE")
+      } else {
+        logs.push(`SET O${position}:${output[position]}`)
       }
     }
   }
@@ -90,10 +96,12 @@ export default function multiply(a: number | string, b: number | string): string
     output.pop()
   }
 
-  const result = output.slice().reverse().join("")
+  const result = output.slice().reverse()
 
-  logs.push(`RETURN ${result}`)
-  logs.push(`REFERENCE ${A * B}`)
+  // logs.push(`${toPositional(output.join(""), "O")}`)
+  // logs.push(result.split("").join(" "))
+  logs.push(`RETURN ${toPositional(result.join(""), "O")}`)
+  logs.push(`REFERENCE ${toPositional(A * B, "O")}`)
 
   return logs.join("\n")
 }
